f62a74a3811dfcece613719ae515c31324fd5314
Maria Heloisa Xavier <118367487+xavierheloisa@users.noreply.github.com>
2023-11-07 21:58:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
-alert( 'cuidado virus');
-<button onclick="alert('Pom')" class="tecla tecla_pom">Pom</button>;
-<audio controls="" src="sounds/keyq.wav" id="som_tecla_pom"></audio>;
+alert('Essa página contém vírus');
+
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play();}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+
+function tocaSomclap(){document.querySelector('#som_tecla_clap').play();}
+
+document.querySelector('.tecla_clap').onclick = tocaSomclap;
+
+
+
+function tocaSomtim(){document.querySelector('#som_tecla_tim').play();}
+
+document.querySelector('.tecla_tim').onclick = tocaSomtim;
+
+
+
+function tocaSompuff(){document.querySelector('#som_tecla_puff').play();}
+
+document.querySelector('.tecla_puff').onclick = tocaSompuff;
+
+
+
+function tocaSomsplash(){document.querySelector('#som_tecla_splash').play();}
+
+document.querySelector('.tecla_splash').onclick = tocaSomsplash;
+
+function tocaSomtoim(){document.querySelector('#som_tecla_toim').play();}
+
+document.querySelector('.tecla_toim').onclick = tocaSomtoim;
+
+function tocaSompsh(){document.querySelector('#som_tecla_psh').play();}
+
+document.querySelector('.tecla_psh').onclick = tocaSompsh;
+
+function tocaSomtic(){document.querySelector('#som_tecla_tic').play();}
+
+document.querySelector('.tecla_tic').onclick = tocaSomtic;
+
+function tocaSomtom(){document.querySelector('#som_tecla_tom').play();}
+
+document.querySelector('.tecla_tom').onclick = tocaSomtom;
+
+const ListaDeTecla = document.querySelectorAll('.tecla')
